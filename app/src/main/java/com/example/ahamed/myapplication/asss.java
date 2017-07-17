65475168fd5059fa8b38ab91f6c28d125f954e12
ahamed <eng.a_hamed@hotmail.com>
2017-07-17 16:23:41 +0200

    this is a second sasd

---
+++ app/src/main/java/com/example/ahamed/myapplication/asss.java
@@ -0,0 +1,17 @@
+package com.example.ahamed.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ahamed on 17/07/2017.
+ */
+
+public class asss extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        //this is a test file
+        super.onCreate(savedInstanceState);
+    }
+}
